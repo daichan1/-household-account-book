1c823961a1d7c4e56d948dd3ac74832c9e12c280
Daisuke Saito <chansa-89.one-line@ezweb.ne.jp>
2021-09-02 11:44:33 +0900

    create income_and_expenditure new_page

--- app/controllers/income_and_expenditures_controller.rb
+++ app/controllers/income_and_expenditures_controller.rb
@@ -1,4 +1,37 @@
 class IncomeAndExpendituresController < ApplicationController
   def index
   end
+
+  def show
+  end
+
+  def new
+    @income_and_expenditure = IncomeAndExpenditure.new
+    @categories = Category.all
+  end
+
+  def create
+    @income_and_expenditure = IncomeAndExpenditure.new(income_and_expenditures_params)
+    if @income_and_expenditure.save
+      redirect_to income_and_expenditures_path, flash: { income_and_expenditures_notice: "収支登録が完了しました" }
+    else
+      @categories = Category.all
+      render "new", collection: @categories
+    end
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def income_and_expenditures_params
+    params.require(:income_and_expenditure).permit(:is_income_and_expenditure, :name, :price, :category_id, :user_id)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
     registrations: 'users/registrations'
   }
   resources :categories, only: [:index, :create, :update, :destroy]
+  resources :income_and_expenditures
   root 'income_and_expenditures#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
