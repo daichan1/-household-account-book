f902abe7b6859578878a7fcfae092c33cc93ceb7
Daisuke Saito <chansa-89.one-line@ezweb.ne.jp>
2021-09-03 18:11:35 +0900

    create income_and_expenditure edit_update

--- app/controllers/income_and_expenditures_controller.rb
+++ app/controllers/income_and_expenditures_controller.rb
@@ -22,9 +22,18 @@ class IncomeAndExpendituresController < ApplicationController
   end
 
   def edit
+    @income_and_expenditure = IncomeAndExpenditure.find(params[:id])
+    @categories = Category.all
   end
 
   def update
+    @income_and_expenditure = IncomeAndExpenditure.find(params[:id])
+    if @income_and_expenditure.update(income_and_expenditures_params)
+      redirect_to income_and_expenditures_path, flash: { income_and_expenditures_notice: "収支の更新が完了しました" }
+    else
+      @categories = Category.all
+      render "edit", collection: @categories
+    end
   end
 
   def destroy
